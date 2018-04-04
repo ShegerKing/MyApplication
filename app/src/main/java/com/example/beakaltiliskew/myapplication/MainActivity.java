a3832fb06e5e9618d72de9b757e10f7d07eece11
Dale <rpmmaniac13@gmail.com>
2018-04-04 15:17:49 -0400

    calendar

--- app/src/main/java/com/example/beakaltiliskew/myapplication/MainActivity.java
+++ app/src/main/java/com/example/beakaltiliskew/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Calendar);
+        setContentView(R.layout.calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
