023e386b70e03359fe44e5e4667afd093f60c961
Sheroman <bktotient@gmail.com>
2018-04-18 14:33:27 -0500

    added settings

--- app/src/main/java/com/example/beakaltiliskew/myapplication/MainActivity.java
+++ app/src/main/java/com/example/beakaltiliskew/myapplication/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.beakaltiliskew.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +25,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Reminder_database Helper = new Reminder_database(getApplicationContext());
+        SQLiteDatabase db=  Helper.getWritableDatabase();
+
+        // Initialize views
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        FloatingActionButton ReminderButton = (FloatingActionButton) findViewById(R.id.add_reminder);
+        RecyclerView list  = (RecyclerView) findViewById(R.id.reminder_list);
+        TextView NoReminder = (TextView) findViewById(R.id.no_reminder_text);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // To check is there are saved reminders
+        // If there are no reminders display a message asking the user to create reminders
+        List<Reminder> mTest = Helper.getAllReminders();
+
+        if (mTest.isEmpty()) {
+            NoReminder.setVisibility(View.VISIBLE);
+        }
+
+        // Create recycler view
+        //
+        list.setLayoutManager(new LinearLayoutManager(this));
+        registerForContextMenu(list);
+        SimpleAdapter Adapter = new SimpleAdapter();
+        //Adapter.setItemCount(getDefaultItemCount());
+       // list.setAdapter(Adapter);
+
+        Adapter.
+
+        // Setup toolbar
+       // setSupportActionBar(toolbar);
+       toolbar.setTitle(R.string.app_name);
+        ReminderButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent modifySettings=new Intent(MainActivity.this,SettingActivity.class);
-                startActivity(modifySettings);
-
+            public void onClick(View v) {
+                //going to implement add a reminder activity here
+              //  Intent intent = new Intent(v.getContext(), add.class);
+               // startActivity(intent);
             }
         });
+
+
+    }
+
+
 
 
     }
@@ -58,4 +96,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
